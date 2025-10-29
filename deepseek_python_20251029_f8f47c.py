a9758f383d1ca5848d1b6df9359314857facde9e
amineTNYT <benabdallahamine2019@gmail.com>
2025-10-29 17:52:20 +0100

    Add files via upload

---
+++ deepseek_python_20251029_f8f47c.py
@@ -0,0 +1,45 @@
+from numpy import array
+
+# Fonction pour saisir et valider la taille du tableau
+def sasir():
+    n=int(input("Donner le taille du tableau=:"))
+    while not (3<=n<=5):
+        n=int(input("Donner taille du tableau=:"))
+    return n
+
+# Fonction pour remplir le tableau avec des valeurs saisies par l'utilisateur
+def remplir(t,n):
+    for i in range(0,n):
+        t[i]=int(input("t["+str(i)+"]=:"))
+ 
+# Fonction de tri utilisant l'algorithme de tri par sélection
+def tri(t,n):
+    for i in range(n):
+        for j in range(i+1,n):
+            if t[i]>t[j]:
+                aux=t[j]
+                t[j]=t[i]
+                t[i]=aux
+    
+            
+# Fonction pour afficher les éléments du tableau séparés par "|"
+def afficher(t,n):
+    for i in range(0,n):
+        print(t[i],end="|")
+   
+ 
+# Programme Principal
+n=sasir()  # Saisie de la taille du tableau
+t=array([int()]*n)  # Création du tableau avec NumPy
+remplir(t,n)  # Remplissage du tableau
+
+# Affichage du tableau avant le tri
+print("le tableau avant le tri")
+afficher(t,n)
+print()
+
+tri(t,n)  # Tri du tableau
+
+# Affichage du tableau après le tri
+print("le tableau aprés le tri")
+afficher(t,n)
