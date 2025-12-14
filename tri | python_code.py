70d3778739f09cd0900e245064ff0089547d7364
amineTNYT <170637819+amineTNYT@users.noreply.github.com>
2025-12-14 10:26:40 +0100

    Update comments to use 'procédure' instead of 'Fonction'

--- tri | python_code.py
+++ tri | python_code.py
@@ -1,18 +1,18 @@
 from numpy import array
 
-# Fonction pour saisir et valider la taille du tableau
+# procédure pour saisir et valider la taille du tableau
 def sasir():
     n=int(input("Donner le taille du tableau=:"))
     while not (3<=n<=5):
         n=int(input("Donner taille du tableau=:"))
     return n
 
-# Fonction pour remplir le tableau avec des valeurs saisies par l'utilisateur
+# procédure  pour remplir le tableau avec des valeurs saisies par l'utilisateur
 def remplir(t,n):
     for i in range(0,n):
         t[i]=int(input("t["+str(i)+"]=:"))
  
-# Fonction de tri utilisant l'algorithme de tri par sélection
+# procédure de tri utilisant l'algorithme de tri par sélection
 def tri(t,n):
     for i in range(n):
         for j in range(i+1,n):
@@ -22,7 +22,7 @@ def tri(t,n):
                 t[i]=aux
     
             
-# Fonction pour afficher les éléments du tableau séparés par "|"
+# procédure pour afficher les éléments du tableau séparés par "|"
 def afficher(t,n):
     for i in range(0,n):
         print(t[i],end="|")
