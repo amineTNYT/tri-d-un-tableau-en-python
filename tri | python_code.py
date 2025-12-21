f57b7cd2cdb6331781437b41261d21ef1f036ff3
amineTNYT <170637819+amineTNYT@users.noreply.github.com>
2025-12-21 15:33:42 +0100

    Update tri | python_code.py
    fixed the function for tri selection

--- tri | python_code.py
+++ tri | python_code.py
@@ -12,15 +12,21 @@ def remplir(t,n):
     for i in range(0,n):
         t[i]=int(input("t["+str(i)+"]=:"))
  
-# procédure de tri utilisant l'algorithme de tri par sélection
-def tri(t,n):
-    for i in range(n):
-        for j in range(i+1,n):
-            if t[i]>t[j]:
-                aux=t[j]
-                t[j]=t[i]
-                t[i]=aux
-    
+# Fonction de tri du tableau (tri par sélection)
+def tri_seléction(t, n):
+    # Parcourir tous les éléments du tableau
+    for i in range(n-1):
+        # Position du minimum actuel
+        pMin = i
+        # Chercher le minimum dans la partie non triée
+        for j in range(i+1, n):
+            if t[j] < t[pMin]:
+                pMin = j
+        # Échanger les éléments si nécessaire
+        if i != pMin:
+            aux = t[i]
+            t[i] = t[pMin]
+            t[pMin] = aux    
             
 # procédure pour afficher les éléments du tableau séparés par "|"
 def afficher(t,n):
